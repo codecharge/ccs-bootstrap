9eb0538359f3121236e451f8e40ae7119509f458
Cléverton Heming <clevertonh@gmail.com>
2014-02-13 17:22:54 -0200

    Updated MasterPage
    Updated MasterPage

--- Designs/CCS-Bootstrap/MasterPage.ccp
+++ Designs/CCS-Bootstrap/MasterPage.ccp
@@ -1,11 +1,12 @@
-<Page id="1" templateExtension="html" relativePath="..\.." fullRelativePath=".\Designs\CCS-Bootstrap" secured="False" urlType="Relative" isIncluded="False" SSLAccess="False" isService="False" cachingEnabled="False" cachingDuration="1 minutes" wizardTheme="None" wizardThemeVersion="3.0" useDesign="False">
+<Page id="1" templateExtension="html" relativePath="..\.." fullRelativePath=".\Designs\CCS-Bootstrap" secured="False" urlType="Relative" isIncluded="False" SSLAccess="False" isService="False" cachingEnabled="False" cachingDuration="1 minutes" wizardTheme="None" wizardThemeVersion="3.0" useDesign="False" needGeneration="0">
 	<Components>
 	</Components>
 	<CodeFiles>
-	</CodeFiles>
+		<CodeFile id="Code" language="PHPTemplates" name="MasterPage.php" forShow="True" url="MasterPage.php" comment="//" codePage="utf-8"/>
+</CodeFiles>
 	<SecurityGroups/>
-<CachingParameters/>
-<Attributes/>
-<Features/>
-<Events/>
+	<CachingParameters/>
+	<Attributes/>
+	<Features/>
+	<Events/>
 </Page>
---
+++ Designs/CCS-Bootstrap/MasterPage.php
@@ -0,0 +1,102 @@
+<?php
+//Include Common Files @1-F7BE3776
+define("RelativePath", "../..");
+define("PathToCurrentPage", "/Designs/CCS-Bootstrap/");
+define("FileName", "MasterPage.php");
+include_once(RelativePath . "/Common.php");
+include_once(RelativePath . "/Template.php");
+include_once(RelativePath . "/Sorter.php");
+include_once(RelativePath . "/Navigator.php");
+//End Include Common Files
+
+//Initialize Page @1-6DC11B93
+// Variables
+$FileName = "";
+$Redirect = "";
+$Tpl = "";
+$TemplateFileName = "";
+$BlockToParse = "";
+$ComponentName = "";
+$Attributes = "";
+
+// Events;
+$CCSEvents = "";
+$CCSEventResult = "";
+$TemplateSource = "";
+
+$FileName = FileName;
+$Redirect = "";
+$TemplateFileName = "MasterPage.html";
+$BlockToParse = "main";
+$TemplateEncoding = "UTF-8";
+$ContentType = "text/html";
+$PathToRoot = "../../";
+$PathToRootOpt = "../../";
+$Scripts = "|";
+//End Initialize Page
+
+//Before Initialize @1-E870CEBC
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeInitialize", $MainPage);
+//End Before Initialize
+
+//Initialize Objects @1-EC146B92
+$Attributes = new clsAttributes("page:");
+$Attributes->SetValue("pathToRoot", $PathToRoot);
+$MainPage->Attributes = & $Attributes;
+$ScriptIncludes = "";
+$SList = explode("|", $Scripts);
+foreach ($SList as $Script) {
+    if ($Script != "") $ScriptIncludes = $ScriptIncludes . "<script src=\"" . $PathToRoot . $Script . "\" type=\"text/javascript\"></script>\n";
+}
+$Attributes->SetValue("scriptIncludes", $ScriptIncludes);
+
+$CCSEventResult = CCGetEvent($CCSEvents, "AfterInitialize", $MainPage);
+
+if ($Charset) {
+    header("Content-Type: " . $ContentType . "; charset=" . $Charset);
+} else {
+    header("Content-Type: " . $ContentType);
+}
+//End Initialize Objects
+
+//Initialize HTML Template @1-97C0D71D
+$CCSEventResult = CCGetEvent($CCSEvents, "OnInitializeView", $MainPage);
+$Tpl = new clsTemplate($FileEncoding, $TemplateEncoding);
+if (strlen($TemplateSource)) {
+    $Tpl->LoadTemplateFromStr($TemplateSource, $BlockToParse, "UTF-8");
+} else {
+    $Tpl->LoadTemplate(PathToCurrentPage . $TemplateFileName, $BlockToParse, "UTF-8");
+}
+$Tpl->SetVar("CCS_PathToRoot", $PathToRoot);
+$Tpl->block_path = "/$BlockToParse";
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeShow", $MainPage);
+$Attributes->SetValue("pathToRoot", "../../");
+$Attributes->Show();
+//End Initialize HTML Template
+
+//Go to destination page @1-FBA93089
+if($Redirect)
+{
+    $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+    header("Location: " . $Redirect);
+    unset($Tpl);
+    exit;
+}
+//End Go to destination page
+
+//Show Page @1-D35A3B77
+$Tpl->block_path = "";
+$Tpl->Parse($BlockToParse, false);
+if (!isset($main_block)) $main_block = $Tpl->GetVar($BlockToParse);
+$main_block = CCConvertEncoding($main_block, $FileEncoding, $TemplateEncoding);
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeOutput", $MainPage);
+if ($CCSEventResult) echo $main_block;
+//End Show Page
+
+//Unload Page @1-74A7C1E7
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+unset($Tpl);
+//End Unload Page
+
+
+?>
